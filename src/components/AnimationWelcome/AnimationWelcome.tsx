c3151d1458c44ea413415f08751437c4f647afbf
Juan Maurente <juanmaurente@Juans-MacBook-Air.local>
2024-07-24 20:03:25 +1000

    installed ngrok for testing ui

--- src/components/AnimationWelcome/AnimationWelcome.tsx
+++ src/components/AnimationWelcome/AnimationWelcome.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
 import { Player } from '@lottiefiles/react-lottie-player';
 import welcomeAnimation from './animationStart.json';
 import styles from './AnimationWelcome.module.css';
 
 const WelcomeAnimation = () => {
+	useEffect(() => {
+		const tl = gsap.timeline();
+		tl.set(`.${styles.animationContainer}`, {
+			opacity: 0,
+		})
+			.to(`.${styles.animationContainer}`, {
+				duration: 1.5,
+				opacity: 1,
+			})
+			.to(`.${styles.animationContainer}`, {
+				duration: 0.5,
+				opacity: 0,
+			});
+	}, []);
+
 	return (
 		<div className={styles.animationContainer}>
 			<Player
